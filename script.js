d329724594df32d8d8d7e34495ef88171f68fac4
Guiihsant01 <guilherme.sants.dev@gmail.com>
2025-06-03 14:39:01 -0300

    finale commit

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#*&¨$%";
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
 let newPassword = "";
 
 sizePassword.innerHTML = sliderElement.value;
